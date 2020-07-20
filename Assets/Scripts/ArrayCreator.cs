d4cac049a02242f5334257cb3b82d10ebf2e9de0
lschiesser <41335130+lschiesser@users.noreply.github.com>
2020-07-20 16:01:38 +0200

    tags added to prefabs, modifications in ArrayCreator.cs

--- Assets/Scripts/ArrayCreator.cs
+++ Assets/Scripts/ArrayCreator.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class ArrayCreator : MonoBehaviour
 {
+    public GameObject[] stimuliPrefabs;
+
+    private List<GameObject> fearInducing;
+    private List<GameObject> animateControl;
+    private List<GameObject> inanimateControl;
     // Start is called before the first frame update
     void Start()
     {
-        
+        fearInducing = new List<GameObject>();
+        animateControl = new List<GameObject>();
+        inanimateControl = new List<GameObject>();
+        foreach (var item in stimuliPrefabs)
+        {
+            if (item.CompareTag("fear"))
+            {
+                fearInducing.Add(item);
+            } else if (item.CompareTag("animate"))
+            {
+                animateControl.Add(item);
+            } else if (item.CompareTag("inanimate"))
+            {
+                inanimateControl.Add(item);
+            }
+        }
     }
 
     // Update is called once per frame
